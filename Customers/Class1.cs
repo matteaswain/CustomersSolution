27cb8e4dfd70caab4fd1b54558e1a60d6d529656
Mattea Swain <mmswain@tql.com>
2021-05-20 12:34:16 -0400

    Add project files.

---
+++ Customers/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customers
+{
+    class Custy // class
+    {
+        private static int NextId { get; set; } = 1; // properties
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal Sales { get; private set; }
+        public bool Active { get; set; } = true;
+
+        public void AddSales(decimal amount) // method
+        {
+            Sales = Sales + amount; // assignment -- parameter acts like variable 
+        }
+
+        public Custy(string Name, string City, string State) // constructor
+        {
+            this.Id = NextId;
+            NextId++;
+            this.Name = Name;
+            this.City = City;
+            this.State = State;    
+        }
+               
+    }
+}
---
+++ Customers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Customers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var custy1 = new Custy("TQL", "Cincinnati", "Ohio");
+            var custy2 = new Custy("USB", "Stockton", "California");
+            var custy3 = new Custy("HelloFresh", "Pittsburg", "Pennsylvania");
+
+            custy1.AddSales(1000);
+            custy1.AddSales(4000.50m); // to signal decimal m is placed after number
+            Console.WriteLine($"Customer {custy1.Name} sales is {custy1.Sales}");
+
+        }
+    }
+}
